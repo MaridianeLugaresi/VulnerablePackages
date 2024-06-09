9b86da26d2b5d96443068e9bf57b492997ec72c1
Maridiane Lugaresi <maridiane.lugaresi@hotmail.com>
2024-06-09 15:31:22 -0300

    Added treatment for FTP protocol and Regex to identify emails

--- src/main.py
+++ src/main.py
@@ -13,6 +13,9 @@ def process_pcap():
             if 'HTTP' in packet:
                 http_protocol(packet)
 
+            if 'FTP' in packet:
+                ftp_protocol(packet)
+
         except AttributeError as e:
             print(f'Error processing package: {e}')
 
@@ -39,12 +42,41 @@ def http_protocol(packet):
             html_content = bytes.fromhex(http_layer.file_data.replace(':', '')).decode('utf-8', errors='replace')
             print(f'Conteúdo HTML:\n{html_content}')
 
+            # Search if content have keywords vulnerable
             for keyword in keywords_vulnerable:
                 if re.search(keyword, html_content, re.IGNORECASE):
                     print(f'Found keyword: {keyword}')
 
         except ValueError as ve:
             print("The package haven't layer HTML")
+
+    print('------------------------------------')
+
+def ftp_protocol(packet):
+
+    # Regular expression to find email pattern
+    email_pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+                               
+    print('Detected FTP protocol')
+
+    ftp_layer = packet.ftp
+    print(f'ID package: {packet.number}')
+    
+    if hasattr(ftp_layer, 'request_command'):
+        print(f'FTP command: {ftp_layer.request_command}')
+
+    if hasattr(ftp_layer, 'request_arg'):
+        print(f'FTP argument: {ftp_layer.request_arg}')
+
+        # Search if arguments have email
+        if email_pattern.search(ftp_layer.request_arg):
+            print(f'Found email address: {ftp_layer.request_arg}')
+
+    if hasattr(ftp_layer, 'response_code'):
+        print(f'FTP response code: {ftp_layer.response_code}')
+
+    if hasattr(ftp_layer, 'response_arg'):
+        print(f'FTP response arg: {ftp_layer.response_arg}')
 
     print('------------------------------------')
 
